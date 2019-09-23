908fab7aea20ba44bca6a7d1170899884dbf18ef
RealmOfTibbles <5703945+RealmOfTibbles@users.noreply.github.com>
2019-09-23 20:10:07 +0100

    Update buildWorkAround.sh

--- buildWorkAround.sh
+++ buildWorkAround.sh
@@ -1,2 +1,8 @@
- /home/steam/Server1/srcds_run -game garrysmod +maxplayers 12 +map gm_flatgrass +gamemode terrortown -console +host_workshop_collection $1 -authkey $2 +quit
- return 0
+#!/usr/bin/env expect 
+
+set collection [lindex $argv 0]
+set API [lindex $argv 1]
+
+spawn /home/steam/Server1/srcds_run -game garrysmod +maxplayers 12 +map gm_flatgrass +gamemode terrortown -console +host_workshop_collection $collection -authkey $API 
+
+expect "VAC secure mode is activated." {close}
